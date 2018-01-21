302d31ddca31f3b50ff51a6dab608a6ca677e5a1
SofiiaTkachuk <31979778+SofiiaTkachuk@users.noreply.github.com>
2018-01-22 00:08:42 +0200

    update test

--- Test/SimpleTest.cs
+++ Test/SimpleTest.cs
@@ -10,29 +10,16 @@ using System.Web;
 namespace Test
 {
     [TestFixture]
-    public class SimpleTest
+    public static class SimpleTest
     {
-        IWebDriver driver;
-
-        [SetUp]
-        public void Initialize()
-        {
-            //FirefoxProfile fp = new FirefoxProfile();
-            //fp.SetPreference("browser.startup.homepage", "about:blank");
-            //fp.SetPreference("startup.homepage_welcome_url", "about:blank");
-            //fp.SetPreference("startup.homepage_welcome_url.additional", "about:blank");
-            IWebDriver driver = new ChromeDriver();
-        }
-
         [TestCase]
-        public void OpenGoogle()
+        public static void OpenGoogle()
         {
-            driver.Url = "https://www.google.com.ua/";
-        }
-
-        [TearDown]
-        public void Close()
-        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("user-data-dir=/path/to/your/custom/profile");
+            options.AddArguments("start-maximized");
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://www.google.com.ua/");
             driver.Close();
         }
     }
